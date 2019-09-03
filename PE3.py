31c9930b5f54b23dde2f2131afe8131893509270
Nick Wade <nrwade0@frostburg.edu>
2019-09-03 19:42:31 -0400

    Upload #3

---
+++ PE3.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Project Euler #3
+Largest Prime Factor
+
+The prime factors of 13195 are 5, 7, 13 and 29.
+
+What is the largest prime factor of the number 600851475143?
+"""
+
+target = 13195
+
