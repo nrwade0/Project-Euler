2b9238786357ffe0d6359637b763bea5224582f1
Nick <nrwade0@gmail.com>
2020-01-25 00:48:53 -0500

    Upload prob 4
    Uncommented

---
+++ MATLAB/1-50/p04.m
@@ -0,0 +1,34 @@
+% 
+% Project Euler 4 - Palindromic products
+% 
+% A palindromic number reads the same both ways. The largest palindrome
+%  made from the product of two 2-digit numbers is 9009 = 91 × 99.
+%
+% Find the largest palindrome made from the product of two 3-digit numbers.
+% 
+
+threes = 100:999;
+max = 0;
+
+for i = 1:length(threes)
+    for j = 1:length(threes)
+        product = threes(i) * threes(j);
+        
+        if(read_palindrome(num2str(product)) == true)
+            if(product > max)
+                max = product;
+            end
+        end
+    end
+end
+
+disp(max)
+
+
+function y = read_palindrome(str)
+    if(str == reverse(str))
+        y = true;
+    else
+        y = false;
+    end
+end
