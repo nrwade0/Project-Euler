7d32a86ea73c9489f70a534cd63a1a09a1c04664
Nick <nrwade0@gmail.com>
2020-01-25 00:51:10 -0500

    Add comments

--- MATLAB/1-50/p04.m
+++ MATLAB/1-50/p04.m
@@ -7,13 +7,17 @@
 % Find the largest palindrome made from the product of two 3-digit numbers.
 % 
 
+% array of all three digit numbers
 threes = 100:999;
 max = 0;
 
 for i = 1:length(threes)
     for j = 1:length(threes)
+        % cycle through all three digit numbers and multiply by a second
+        % loop
         product = threes(i) * threes(j);
         
+        % if number is a palindrome, see function, set new max
         if(read_palindrome(num2str(product)) == true)
             if(product > max)
                 max = product;
@@ -22,9 +26,10 @@ for i = 1:length(threes)
     end
 end
 
+% display answer
 disp(max)
 
-
+% function determines if a string 'str' is a palindrome, uses reverse func
 function y = read_palindrome(str)
     if(str == reverse(str))
         y = true;
