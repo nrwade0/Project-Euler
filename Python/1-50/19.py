a524e185913419e75c827ab3e3c36d2f7f11b563
Nick <nrwade0@gmail.com>
2020-04-10 12:06:33 -0400

    Fixed Counting Sundays
    Added comments and simplified

--- Python/1-50/19.py
+++ Python/1-50/19.py
@@ -1,52 +1,49 @@
 """
-Created on Tue Oct  8 19:06:18 2019
+Project Euler 19 - Counting Sundays
 
-You are given the following information, but you may prefer to do some research for yourself.
-
-1 Jan 1900 was a Monday.
-
-Thirty days has September,
-April, June and November.
-All the rest have thirty-one,
-Saving February alone,
-Which has twenty-eight, rain or shine.
-And on leap years, twenty-nine.
-
-A leap year occurs on any year evenly divisible by 4, but not on a century unless it is divisible by 400.
-
-How many Sundays fell on the first of the month during the twentieth century (1 Jan 1901 to 31 Dec 2000)?
+    You are given the following information, but you may prefer to do some
+      research for yourself.
+    
+    1 Jan 1900 was a Monday.
+    
+    Thirty days has September,
+    April, June and November.
+    All the rest have thirty-one,
+    Saving February alone,
+    Which has twenty-eight, rain or shine.
+    And on leap years, twenty-nine.
+    
+    A leap year occurs on any year evenly divisible by 4, but not on a century
+      unless it is divisible by 400.
+    
+    How many Sundays fell on the first of the month during the twentieth 
+      century (1 Jan 1901 to 31 Dec 2000)?
 """
+    
+# List of days in each month - leap year added later
+months = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
+
+# counting variables
+sunday_count = 0
+day = 1
+
+# cycle thru each 'year' in the 20th century
+for year in range(1901, 2000):
+    
+    # cycle thru the months 'm' in each year
+    for m in months:
+        
+        # running count of days, add the number of days in a month for current
+        #  first day of the month
+        day += m
+        
+        # calculate leap year month
+        if year % 4 == 0 and m == 28:
+            day += 1
+        
+        # if it is a sunday, add it to count
+        if day % 7 == 0:
+            sunday_count += 1
 
 
-import numpy as np
-from itertools import islice, cycle
-
-# cycle thru the weekdays choosing days that begin on the first of each month?
-days = ['mon', 'tues', 'wed', 'thurs', 'fri', 'sat', 'sun']
-
-f_noleap = [0, 0+31, 0+31+28, 0+31+28+31, 0+31+28+31+30, 0+31+28+31+30+31, \
-                 0+31+28+31+30+31+30, 0+31+28+31+30+31+30+31, 0+31+28+31+30+31+30+31+31, \
-                 0+31+28+31+30+31+30+31+31+30, 0+31+28+31+30+31+30+31+31+30+31, \
-                 0+31+28+31+30+31+30+31+31+30+31+31]
-
-f_leap = [0, 0+31, 0+31+29, 0+31+29+31, 0+31+29+31+30, 0+31+29+31+30+31, \
-                 0+31+29+31+30+31+30, 0+31+29+31+30+31+30+31, 0+31+29+31+30+31+30+31+31, \
-                 0+31+29+31+30+31+30+31+31+30, 0+31+29+31+30+31+30+31+31+30+31, \
-                 0+31+29+31+30+31+30+31+31+30+31+31]
-
-
-"""
-0
-jan 31 \
-feb 28 \
-mar 31 \
-apr 30 \
-may 31 \
-jun 30 \
-jul 31 \
-aug 31 \
-sep 30 \
-oct 31 \
-nov 31 \
-dec 31
-"""
+print('Sundays:', sunday_count)
